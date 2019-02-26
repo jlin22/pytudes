6275b9c03d7426a55faa1d225f471c70aed8627d
John Lin <jlin1674@gmail.com>
2019-02-26 15:03:36 -0500

    parser for lisp

--- lisp.py
+++ lisp.py
@@ -7,11 +7,34 @@ Exp = (Atom, List)
 def tokenize(chars: str) -> list:
     return chars.replace('(', ' ( ').replace(')', ' ) ').split()
 
-def parse():
-    pass
+def parse(program: str) -> Exp:
+    return read_from_tokens(tokenize(program))
+
+def read_from_tokens(tokens: list) -> Exp:
+    if len(tokens) == 0:
+        raise SyntaxError('unexpected EOF')
+    token = tokens.pop(0)
+    if token == '(':
+        L = []
+        while tokens[0] != ')':
+            L.append(read_from_tokens(tokens))
+        tokens.pop(0)
+        return L
+    elif token == ')':
+        raise SyntaxError('unexpected )')
+    else:
+        return atom(token)
+
+def atom(token: str) -> Atom:
+    try: return int(token)
+    except ValueError:
+        try: return float(token)
+        except ValueError: 
+            return Symbol(token)
 
 def eval():
     pass
 
 program = '(begin (define r 10) (* pi (* r r)))'
 print(tokenize(program))
+print(parse(program))
