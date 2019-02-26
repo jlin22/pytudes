ab3fdf72bcc5dfdf459148241fdf0f0502f3a2d7
John Lin <jlin1674@gmail.com>
2019-02-26 15:20:08 -0500

    done with basic lisp

--- lisp.py
+++ lisp.py
@@ -1,3 +1,6 @@
+import math
+import operator as op
+
 Symbol = str
 Number = (int, float)
 Atom = (Symbol, Number)
@@ -32,9 +35,55 @@ def atom(token: str) -> Atom:
         except ValueError: 
             return Symbol(token)
 
-def eval():
-    pass
+def standard_environment():
+    env = {}
+    env.update(vars(math))
+    env.update({
+        '+': op.add, '-': op.sub, '*': op.mul, '/': op.truediv,
+        '>': op.gt, '<': op.lt, '>=': op.ge, '<=': op.le, '=': op.eq,
+        'abs': abs,
+        'append': op.add,
+        'apply': lambda proc, args: proc(*args),
+        'begin': lambda *x: x[-1],
+        'car': lambda x: x[0],
+        'cdr': lambda x: x[1:],
+        'cons': lambda x, y: [x] + y,
+        'eq?': op.is_,
+        'equal?': op.eq,
+        'length': len,
+        'list': lambda *x: list(x),
+        'list?': lambda x: isinstance(x, list),
+        'map': lambda *args: list(map(*args)),
+        'max': max,
+        'min': min,
+        'not': op.not_,
+        'null?': lambda x: x == [],
+        'number?': lambda x: isinstance(x, Number),
+        'procedure?': callable,
+        'round': round,
+        'symbol?': lambda x: isinstance(x, Symbol),
+    })
+    return env
+
+global_env = standard_environment()
+
+def eval(x: Exp, env=global_env):
+    print(x)
+    if isinstance(x, Symbol):
+        return env[x]
+    elif isinstance(x, Number):
+        return x
+    elif x[0] == 'if':
+        (_, test, conseq, alt) = x
+        exp = (conseq if eval(test, env) else alt)
+        return eval(exp, env)
+    elif x[0] == 'define':
+        (_, symbol, exp) = x
+        env[symbol] = eval(exp, env)
+    else:
+        proc = eval(x[0], env)
+        args = [eval(arg, env) for arg in x[1:]]
+        return proc(*args)
 
 program = '(begin (define r 10) (* pi (* r r)))'
-print(tokenize(program))
-print(parse(program))
+print(eval(parse(program)))
